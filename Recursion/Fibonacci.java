3ae88d36a098c24a74449dc0c1ca52271e18dd16
sandipjha762 <sandip.jha@gmail.com>
2023-09-25 00:08:21 +0530

    added code

--- Recursion/Fibonacci.java
+++ Recursion/Fibonacci.java
@@ -9,7 +9,9 @@ public class Fibonacci {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
+        int arr[] = new int[n+1];
         System.out.println(fib(n));
+        System.out.println(fibMomo(n, arr));
 
     }
 
@@ -20,5 +22,22 @@ public class Fibonacci {
         }
 
         return fib(n - 1) + fib(n - 2);
-    }
+    } // TC 2^n
+
+    /*
+     * DP Memoization Technique
+     */
+
+     static int fibMomo( int n, int res[]){
+
+        if(n == 0 || n == 1)
+            return n;
+
+        if(res[n] != 0){
+            return res[n];
+        }
+        int result = fibMomo(n-1, res) + fibMomo(n-2, res);
+        res[n] = result;
+        return res[n];
+     }
 }
